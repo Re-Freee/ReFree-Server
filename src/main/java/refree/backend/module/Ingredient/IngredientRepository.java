835e060de1f8d8bbb3970cb7682843eb98143d52
ts3083 <ts04031@gmail.com>
2023-08-05 23:31:29 +0900

    [Update] 재료 등록, 수정, 검색 API 수정

--- src/main/java/refree/backend/module/Ingredient/IngredientRepository.java
+++ src/main/java/refree/backend/module/Ingredient/IngredientRepository.java
@@ -3,6 +3,6 @@ package refree.backend.module.Ingredient;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IngredientRepository extends JpaRepository<Ingredient, Integer>,IngredientRepositoryCustom {
+public interface IngredientRepository extends JpaRepository<Ingredient, Long>,IngredientRepositoryCustom {
 
 }
